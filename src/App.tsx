2fec255cd5286e996e034b6a64f4d5b062ec0ff0
Matts-R <matheushrdasilva13@gmail.com>
2022-07-20 22:40:44 -0300

    Project started

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+      <Navbar/>
+      <hr />
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/about" element={<About />}></Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
